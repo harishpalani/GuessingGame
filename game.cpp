b671b652ba31fc8ccd42c6f4bf11aa0fe5698b96
Harish Palani <harishpalani3@gmail.com>
2016-09-14 04:35:07 +0000

    Guessing Game — comments added

--- game.cpp
+++ game.cpp
@@ -1,3 +1,27 @@
+/**
+ * Guessing Game
+ * 
+ * INTERFACE
+ * The computer will pick a random number between 0 and 100, inclusive. The user will guess a number,
+ * and the computer will tell you if it is too high, too low, or correct. If it is correct, it will display how
+ * many guesses it took you, and ask if you want to play again.
+ * 
+ * IMPLEMENTATION
+ * Create a variable which will hold the random number and use rand() to create it. (5 points) Note that
+ * you need to use srand() to set the random seed first. I would use time (so the line would be
+ * “srand(time(NULL));”) Use cout to print things out to the command line. (5 points) Use cin to read
+ * in a line (5 points):
+ * 
+ * Compare the input to the random number. (5 points) If the guess is correct, print out the number of
+ * guesses (use a variable to keep track) and ask if they want to play again. (5 points) If so, restart the game. (5
+ * points) (You can use y/n for the answer.)
+ *
+ * Comments! (10 points)
+ * 
+ * @author Harish Palani
+ * @version 1.0
+ */
+
 #include <iostream>
 
 using namespace std;
@@ -42,6 +66,8 @@ void playGame() {
     if (guess == r) {
         int input = 0;
         count++;
+        
+        // Users can restart the game
         cout << "You got it! / Guesses required: " << count << "\n" << "Would you like to play again? (0 = no / 1 = yes): ";
         cin >> input;
         if (input == 1) {
